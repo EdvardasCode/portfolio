99937740a30d6524dbd7d79fafd9a75ffd5809a5
Edvardas <edvardas.kozlovas@gmail.com>
2023-09-03 22:24:56 +0100

    initial files

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import "./style.css"
+import ReactDOM from "react-dom/client"
+import { Canvas } from "@react-three/fiber"
+import Experience from "./Experience.jsx"
+import { OrbitControls } from "@react-three/drei"
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+root.render(
+  <Canvas
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 2000,
+    }}
+  >
+    <OrbitControls makeDefault />
+    <Experience />
+  </Canvas>
+)
