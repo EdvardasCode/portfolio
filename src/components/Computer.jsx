7a447551bea6a9c2c92496904777448a368b7fbc
Edvardas <edvardas.kozlovas@gmail.com>
2023-09-07 23:14:55 +0100

    add leva and import updated models

--- src/components/Computer.jsx
+++ src/components/Computer.jsx
@@ -1,11 +1,40 @@
+import React, { useRef } from "react"
 import { useGLTF } from "@react-three/drei"
+import { useControls } from "leva"
 
-export default function Computer({ position }) {
-  const computer = useGLTF("computer.glb")
+export default function Computer(props) {
+  const { nodes, materials } = useGLTF("/monitor.glb")
+  const { nodes: cable } = useGLTF("/cables.glb")
+
+  const { cable_color, monitor_color } = useControls({
+    cable_color: "#ffffff",
+    monitor_color: "#ffffff",
+  })
 
   return (
-    <>
-      <primitive object={computer.scene} scale={1} position={position} />
-    </>
+    <group {...props} dispose={null}>
+      <group position={[0.066, 0.352, -0.28]} scale={0.051}>
+        <mesh castShadow receiveShadow geometry={nodes.Cube103.geometry}>
+          <meshStandardMaterial color={monitor_color} />
+        </mesh>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube103_1.geometry}
+          material={materials.br}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={cable.cable.geometry}
+          position={[0.11, -0.5, -1.4]}
+          scale={6}
+        >
+          <meshStandardMaterial color={cable_color} />
+        </mesh>
+      </group>
+    </group>
   )
 }
+
+useGLTF.preload("/monitor.glb")
