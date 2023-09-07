584bbea1926aa8a5f05ad97942ba891ba36b0f14
Edvardas <edvardas.kozlovas@gmail.com>
2023-09-07 19:10:09 +0100

    add components to expirience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,20 @@
-import { useGLTF, Environment } from "@react-three/drei"
+import { useGLTF } from "@react-three/drei"
+import Computer from "./components/Computer"
 import PictureKobe from "./components/PictureKobe"
 import PictureKyoto from "./components/PictureKyoto"
+import PotTree from "./components/PotTree"
 
 export default function Experience(props) {
   const gltf = useGLTF("desk-set.glb")
   const wall = useGLTF("wall.glb")
   return (
     <>
-      <Environment preset="city" />
+      <ambientLight intensity={0.5} />
+      <pointLight color="beige" position={[0, 2, 3]} intensity={0.6} />
       <PictureKobe position={[-0.7, 0.2, -0.53]} />
       <PictureKyoto position={[-0.1, 0.5, -0.53]} />
+      <PotTree position={[-0.1, -0.56, 0.1]} />
+      <Computer position={[0, -0.5, 0]} />
       <primitive object={gltf.scene} scale={1} position-y={-0.5} />
       <primitive object={wall.scene} scale={1} position-y={-0.5} />
     </>
