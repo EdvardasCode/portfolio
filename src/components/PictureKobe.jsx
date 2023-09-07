b56952fcc40158cd04615aced0fd1e8ad12bbedc
Edvardas <edvardas.kozlovas@gmail.com>
2023-09-07 19:10:34 +0100

    add occlusion to html elements

--- src/components/PictureKobe.jsx
+++ src/components/PictureKobe.jsx
@@ -6,7 +6,12 @@ export default function PictureKobe({ position }) {
   return (
     <>
       <primitive object={pictureKobe.scene} scale={1.3} position={position}>
-        <Html transform={true} position={[0, 0.025, 0]} distanceFactor={1}>
+        <Html
+          transform={true}
+          position={[0, 0.025, 0.001]}
+          distanceFactor={1}
+          occlude="blending"
+        >
           <img src="Kobe.gif" width={132} />
         </Html>
       </primitive>
--- src/components/PictureKyoto.jsx
+++ src/components/PictureKyoto.jsx
@@ -8,8 +8,9 @@ export default function PictureKyoto({ position }) {
       <primitive object={pictureKyoto.scene} scale={1.6} position={position}>
         <Html
           transform={true}
-          position={[-0.011, -0.005, 0]}
+          position={[-0.011, -0.005, 0.001]}
           distanceFactor={1}
+          occlude="blending"
         >
           <img src="Kyoto.jpg" width={77} />
         </Html>
